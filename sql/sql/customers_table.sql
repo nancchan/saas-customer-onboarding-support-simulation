cf1e7c6446f95b8318a647bc0a36465b6e9fb616
nancchan <nancy.chang3887@gmail.com>
2026-06-04 23:06:41 -0700

    Update customers_table.sql

--- sql/sql/customers_table.sql
+++ sql/sql/customers_table.sql
@@ -1,1 +1,10 @@
+-- SaaS Data Layer: Customer Table Creation
 
+CREATE TABLE customers (
+    id INT,
+    first_name TEXT,
+    last_name TEXT,
+    email TEXT,
+    phone TEXT,
+    company TEXT
+);
